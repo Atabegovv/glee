bb4d5ecee0f2d79b77570af7dbe0db296cda0e01
Atabegovv <atabegov98@mail.ru>
2024-04-26 13:30:55 +0300

    slider and header-info

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,51 @@
 "use strict";
-$(function () {
-  
-});
+
+const swiper = new Swiper('.swiper',{
+    // стрелки
+    navigation:{
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+    },
+    // navigation
+    pagination:{
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+    //scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+    grabCursor: true,
+    keyboard:{
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+    },
+    mousewheel:{
+      sensitivity: 1,
+    },
+    slideToClickedSlide: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 0,
+    watchOverflow: true,
+    centeredSlides: true,
+    speed: 1000,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        stopOnLastSlide: true,
+        disableOnInteraction: false,
+    },
+    // effect: "fade",
+
+    // breakpoints:{
+    //   320:{slidesPerview: 1, slidesPerGroup: 1,},
+    //   480:{slidesPerview: 2, slidesPerGroup: 2,},
+    //   990:{slidesPerview: 3, slidesPerGroup: 3,},
+    // },
+  });
